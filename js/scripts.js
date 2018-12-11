d3b158b4927a48924f6789cf0438a83940c41ba5
Sharon Olago <adino@lisheliving.com>
2018-12-11 13:20:56 +0300

    added if statement and hide function on js

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,39 @@
 // var score = 0;
 //
 $(document).ready(function(){
-  
+  $("#checkAnswer").click(function(){
+    var answer=[]
+    answer.push($("input:radio[name=q1]:checked").val());
+    answer.push($("input:radio[name=q2]:checked").val());
+    answer.push($("input:radio[name=q3]:checked").val());
+    answer.push($("input:radio[name=q4]:checked").val());
+    answer.push($("input:radio[name=q5]:checked").val());
+
+      var total = 0;
+      if (answer[0] == "10"){
+        total += 10;
+      }
+      if (answer[1] == "10"){
+        total += 10;
+      }
+      if (answer[2] == "10"){
+        total += 10;
+      }
+      if (answer[3] == "10"){
+        total += 10;
+      }
+      if (answer[4] == "10"){
+        total += 10;
+      }
+    $("#total").val(total);
+  })
+  $("#checkAnswer").click(function() {
+    $(".well").hide();
+    $("#hide").show();
+  });
 })
 
-  for (var i = 0; i<questions.length; i++) {
-    var response == radio[i].check;
-    if(response == radio.correct){
-      score++;
-    }
-    }
+
 
 
 
